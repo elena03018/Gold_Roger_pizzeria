7d0779f938aaff326201dc6af90a4e9fd5fffe08
elena03018 <elenabellu85@gmail.com>
2024-05-24 15:18:45 +0200

    spint2-ticket4-feat: Système de réinitialisation de mot de passe en place

---
+++ src/Form/ResetPasswordRequestFormType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class ResetPasswordRequestFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => ['autocomplete' => 'email'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your email',
+                    ]),
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => "L'e-mail doit contenir au maximum 255 caractères."
+                    ]),
+                    new Email([
+                        'message' => "Veuillez entrer un email valide."
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
