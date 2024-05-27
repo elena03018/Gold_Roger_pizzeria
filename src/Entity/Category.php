a13cc2d26cd202c9fa1ce2b8e7aecb592ab03d36
elena03018 <elenabellu85@gmail.com>
2024-05-27 20:02:34 +0200

    sprint3-ticket2-feat: Gestion des catégories en place

---
+++ src/Entity/Category.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CategoryRepository;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Contraints\UniqueEntity;
+
+
+#[UniqueEntity('name', message: "Cette catégorie existe déjà.")]
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    // Nom
+    #[Assert\NotBlank(message: "Le nom est obligatoire.")]
+    #[Assert\Length( 
+        max: 255,
+        maxMessage: 'Le nom ne doit pas dépasser {{ limit }} caractères.',
+    )]
+    
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $name = null;
+
+
+    #[Gedmo\Slug(fields: ['name'])]
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $slug = null;
+
+    #[Gedmo\Timestampable(on: 'create')]
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[Gedmo\Timestampable(on: 'update')]
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    // questa
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    // questa
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    // questa
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
