51ced9841aed9ab162466c21ce154c8da0388778
elena03018 <elenabellu85@gmail.com>
2024-06-17 12:25:29 +0200

    sprint4-ticket1-add: Webpackencore en place

---
+++ src/Form/EditUserRolesFormType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class EditUserRolesFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+            ->add('roles', ChoiceType::class, [
+                'contraints' => [
+                    new NotBlank([
+                        "message" => "Le rôle est obligatoire."
+                    ])
+                ],
+                'choices'  => [
+                    'Rôle utilisateur' => "ROLE_USER",
+                    'Rôle administrateur' => "ROLE ADMIN",
+                ],
+                'expanded'=> false,
+                'multiple' => true
+            ]);
+            
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
