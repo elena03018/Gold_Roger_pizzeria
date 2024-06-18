dfea28a455db5633e95eeb3d767f8e9bc42587e8
elena03018 <elenabellu85@gmail.com>
2024-06-18 14:56:26 +0200

    sprint3_ticket8_feat_user_roles_management

---
+++ src/Form/EditUserRolesFormType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Choice;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class EditUserRolesFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Select a Role'
+                    ]),
+                    new Choice([
+                        'choices' => ['ROLE_USER', 'ROLE ADMIN' ],
+                        'message' => 'Please select a valid rôle',
+                    ])  
+                ],
+                
+                'choices'  => [
+                    'Rôle utilisateur' => "ROLE_USER",
+                    'Rôle administrateur' => "ROLE ADMIN",
+                ],
+                'expanded'=> false,
+                'multiple' => true
+            ]);
+            
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
